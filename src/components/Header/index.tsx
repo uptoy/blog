67523bb221159f0f523e391c72fea207a0ce6152
uptoy <u24to45y@gmail.com>
2021-01-07 23:32:22 +0900

    first commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+const Header = (props:any) => {
+    return (
+        <header className="header">
+          <nav className="headerMenu">
+            <a href="#" >Home</a>
+            <a href="#" >About Us</a>
+            <a href="#" >Contact Us</a>
+          </nav>
+        </header>
+      )
+}
+
+export default Header
