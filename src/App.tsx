5ad37c517f62c65f4c9eb37d0578e6f6e8f1b652
uptoy <u24to45y@gmail.com>
2021-01-08 16:18:15 +0900

    [Add]BlogPost,SideBar

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import './App.css';
 import Home from './containers/Home'
 import Header from './components/Header'
 import TopMedia from './components/'
+import {BrowserRouter as Router,Switch,Route } from 'react-router-dom'
+import ContactUS from './containers/ContactUS'
+import Post from './containers/Post'
 
 const  App:React.FC= () => {
   return (
---
+++ src/components/BlogPost/index.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import './style.css'
+import Card from '../UI/Card'
+import blogPost from '../../data/blog.json'
+
+const  BlogPost:React.FC= (props) => {
+    const [post,setPost] = useState({
+        id:"",
+        blogCategory:"",
+        blogTitle:"",
+        postedOn:"",
+        author:"",
+        blogImage:"",
+        blogText:""
+    })
+    const [postId,setPostId] = useState('')
+
+    useEffect(()=>{
+        const postId =props.match.params.postId;
+        const post = blogPost.data.find(post=>post.id == postId)
+        setPost(post)
+        setPostId(postId)
+    },[post,props.match.params.postId])
+
+    return (
+        <div className="blogPostContainer">
+            <Card >
+            <div className="blogHeader">
+                <span className="blogCategory">{post.blogCategory}</span>
+                <h1 className="postTitle">{post.blog.Title}</h1>
+                <span className="postedBy">posted on {props.postedOn} by {post.author}</span>
+            </div>
+            <div className="postImageContainer">
+                <img src={require('../../blogPostImages/' + post.blogImage)} alt='Post Image'/>
+            </div>
+            <div className="postContent">
+                <h3>{post.blogTitle}</h3>
+                <p>{post.blogText}</p>
+            </div>
+            </Card>
+        </div>
+    )
+}
+
+export default BlogPost
